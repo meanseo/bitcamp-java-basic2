49ffde57af041e0732fead0ab57284275be21a32
meanseo <chlalstjok97@gmail.com>
2022-02-07 20:56:38 +0900

    20:56 Feb07

--- java/com/example/demo/service/Feb07Service.java
+++ java/com/example/demo/service/Feb07Service.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+
 import java.util.Scanner;
 
 /**
--- java/com/example/demo/service/Feb07ServiceImpl.java
+++ java/com/example/demo/service/Feb07ServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+
 import java.util.Scanner;
 
 /**
@@ -27,7 +28,23 @@ public class Feb07ServiceImpl implements Feb07Service{
 
     @Override
     public void getPrime(Scanner scanner) {
-
+        System.out.println("두 수를 입력하시오.");
+        System.out.println("num 1");
+        int num1 = scanner.nextInt();
+        System.out.println("num 2");
+        int num2 = scanner.nextInt();
+        int count=0;
+        for(int i = num1; i <= num2; i++) {
+            for(int j=2; j<=i; j++) {
+                if(i%j ==0) {
+                    count ++;
+                }
+            }
+            if(count==1){
+                System.out.println(i+" ");
+            }
+            count=0;
+        }
     }
 
     @Override
