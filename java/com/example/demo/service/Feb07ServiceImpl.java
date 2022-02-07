397214dd4c45a76ae570a0beb781014048066168
meanseo <chlalstjok97@gmail.com>
2022-02-08 00:32:03 +0900

    2022-02-08 Feb7 수정

--- java/com/example/demo/service/Feb07ServiceImpl.java
+++ java/com/example/demo/service/Feb07ServiceImpl.java
@@ -18,8 +18,30 @@ public class Feb07ServiceImpl implements Feb07Service{
 
     @Override
     public void getDice(Scanner scanner) {
-
-    }
+        String s = "";
+        int num = (int) (Math.random() * 6) + 1;
+        switch (num) {
+            case 1:
+                s = "1번";
+                break;
+            case 2:
+                s = "2번";
+                break;
+            case 3:
+                s = "3번";
+                break;
+            case 4:
+                s = "4번";
+                break;
+            case 5:
+                s = "5번";
+                break;
+            case 6:
+                s = "6번";
+                break;
+        }
+        System.out.println(s);
+        }
 
     @Override
     public void getRps(Scanner scanner) {
@@ -49,11 +71,47 @@ public class Feb07ServiceImpl implements Feb07Service{
 
     @Override
     public void getLeapYear(Scanner scanner) {
+        int year = scanner.nextInt();
+        String res = "";
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            res = year + "년은 윤년";
+        } else {
+            res = year + "년은 평년";
+        }
+        System.out.println(res);
+        /**
+         * 윤년은 해당 연도를 4로 나눈 값이 0임을 의미한다.
+         * 그중 100으로 나눈 값이 0인 것은 윤년에서 제외하나
+         * 400으로 나눈 값이 0인 것은 윤년으로 포함된다.
+         * 그 외의 나머지 연도는 평년이다.
+         * year % 4 == 0  year의 값은 4로 나눈 값이 0이여야한다.
+         * &&                그리고(and)
+         * year % 100 != 0   year의 값은 100으로 나누었을 때 0이 되면 안된다 (!는 부정의 의미)
+         * ||               혹은 (or)
+         * year % 400 == 0 year의 값은 400으로 나눈 값이 0이여야한다.
+         *
+         * else  {res = year + "년은 평년";} 이 외의 값은 모두 +년은 평년으로 출력된다.
+         */
 
     }
 
     @Override
     public void getGuessNumber(Scanner scanner) {
+        int answer = (int) (Math.random() * 100 + 1);
+        int input;
 
+        while (true) {
+            System.out.println("1~100 사이 자연수 입력");
+            input = scanner.nextInt();
+
+            if (answer > input) {
+                System.out.println("Up");
+            } else if (answer < input) {
+                System.out.println("Down");
+            } else {
+                System.out.println("정답");
+                break;
+            }
+        }
     }
 }
